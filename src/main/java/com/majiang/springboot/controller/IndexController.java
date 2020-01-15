eef02113df259f4fb120f308a4d0206f113094cd
haoshuai <2339654012@qq.com>
2020-01-15 11:15:19 +0800

    token save to h2

--- src/main/java/com/majiang/springboot/controller/IndexController.java
+++ src/main/java/com/majiang/springboot/controller/IndexController.java
@@ -1,13 +1,34 @@
 package com.majiang.springboot.controller;
 
+import com.majiang.springboot.mapper.UserMapper;
+import com.majiang.springboot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping({"/","index.html"})
-    public String index(){
+    //request里可以获取cookie信息,用来拿取存入cookie的token
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if("token".equals(cookie.getName())){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(null!=user){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
